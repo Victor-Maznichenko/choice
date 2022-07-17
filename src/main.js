1156a11b6606383a048e519cf01586cec496d389
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-07-17 13:36:23 +0300

    выаываыва

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+import 'normalize.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'normalize.css'
 
 createApp(App).use(router).mount('#app')
