3e88f63a0e13557461ddd3a22f19bd197346983d
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-07-17 23:08:37 +0300

    sdfsdfdsf

--- src/components/ProductCard/CarFuel.vue
+++ src/components/ProductCard/CarFuel.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="card__properties-fuel">
-        <img src="@/assets/images/icons/thereIsNotFuel.svg" alt="" v-if="isSelectOpen == 0">
-        <img src="@/assets/images/icons/thereIsFuel.svg" alt="" v-if="isSelectOpen > 0">
+    <div class="fuel">
+        <img class="fuel__img" src="@/assets/images/icons/thereIsNotFuel.svg" alt="" v-if="fuel == 0">
+        <img class="fuel__img" src="@/assets/images/icons/thereIsFuel.svg" alt="" v-if="fuel > 0">
+        <span class="fuel__count"><b>{{fuel}}</b>/999</span>
     </div>
 </template>
 
@@ -11,3 +12,23 @@ export default {
     props: ['fuel']
 }
 </script>
+
+<style lang="sass">
+.fuel
+    display: flex
+    align-items: center
+    &__img
+        display: block
+        width: unit(13)
+        height: unit(14)
+        margin-right: unit(6)
+    &__count
+        line-height: 100%
+        font-size: unit(11)
+        color: #8285B4
+        b
+            font-size: unit(12)
+            font-weight: 500
+            color: #FFFFFF
+            
+</style>
